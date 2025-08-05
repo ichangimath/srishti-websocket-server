8c379a4b96049bc51ce5a77c346066eea33dc55f
Prayag P Ichangimath <prayag@ichangimath.org>
2025-08-05 09:01:34 +0530

    v1

---
+++ server.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+const PORT = process.env.PORT || 3000;
+
+const server = new WebSocket.Server({ port: PORT });
+
+server.on('connection', socket => {
+  console.log('Client connected');
+
+  socket.on('message', message => {
+    console.log(`Received: ${message}`);
+    socket.send(`Echo: ${message}`);
+  });
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log(`WebSocket server running on ws://localhost:${PORT}`);
